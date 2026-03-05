bc8dcc57b259c406b529d133ede1a77935378771
Luis Diego Aparicio Castro <luisdiegoapariciocastro@gmail.com>
2026-03-05 13:30:08 -0600

    Luis Diego Aparicio Castro: agrego mi linea

--- vacio.py
+++ vacio.py
@@ -1,2 +1,3 @@
 "Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
 print("Nombre: Carlos Macías Ruiz | Carrera: Ing. Automotriz | Hobby: Jugar videojuegos")
+print("Nombre: Luis Diego Aparicio Castro | Carrera: Ing. Automotriz | Hobby: Dormir")
