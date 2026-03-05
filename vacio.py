e9921744913c1fbafc8500b14d15a5b4f9ec58a7
Smart Parking System ESP32 <juancarlosgolopz@gmail.com>
2026-03-05 14:15:53 -0600

    Juan Carlos: agrego mi línea

--- vacio.py
+++ vacio.py
@@ -1,11 +1,16 @@
-print("Nombre: ian cota | Carrera: Ing. Automotriz | Hobby: jugar futbol")
-print("Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
-print("Nombre: Carlos Macías Ruiz | Carrera: Ing. Automotriz | Hobby: Jugar videojuegos")
-print("Nombre: Luis Diego Aparicio Castro | Carrera: Ing. Automotriz | Hobby: Dormir")
-print("Nombre: luis angel araiza suarez | Carrera: Ing. Automotriz | Hobby: Jugar trabajar en autos")
-print("Nombre: Santiago Soria Alvarez | Carrera: Ing. Automotriz | Hobby: Jugar futbol")
-print("Nombre: Santiago Valle Garnica | Carrera: Ing. Automotriz | Hobby: Hacer deporte")
-print("Nombre: Cesar | Carrera: Ing. Automotriz | Hobby: musica")
-print("Nombre: Tu joan | Carrera: Ing. Automotriz | Hobby: videojuegos")
-print("Nombre: Cristian Ivan Fragoso Zamudio | Carrera: Ing. Automotriz | Hobby: hacer deporte")
-print("Nombre: Roberto Montaño Limón | Carrera: Ing. Automotriz | Hobby: videojuegos")
+"Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Nombre: Juan Carlos | Carrera: Ing. Automotriz | Hobby: Futbol")
+
